8b79d3fb03bbd63a116d1d2434c7b34027b349f2
Bilal <noreldenb1@montclair.edu>
2018-11-30 23:29:04 -0500

    PlatForces
    Each of the Platforms has their own forces, left platform force to the right and right platform force to the left.

--- Assets/Scripts/Balldirection.cs
+++ Assets/Scripts/Balldirection.cs
@@ -22,9 +22,21 @@ public class Balldirection : MonoBehaviour {
         if(collision.gameObject.tag == "Ball") // I forgot that tags existed! It just occured to me that a tag would work better
         {                                      // here since the name of each clone could be different.
             rb = collision.gameObject.GetComponent<Rigidbody>();
+
             rb.constraints = RigidbodyConstraints.FreezePositionZ;
+            
+            if (this.name == "Left Hand")
+            { 
+                rb.AddForce(Vector3.right * 2, ForceMode.Impulse);
+                //rb.AddForce(Vector3.up * 2, ForceMode.Impulse); // no need
+            }
+
+            else
+            { 
+                rb.AddForce(Vector3.left * 2, ForceMode.Impulse);
+
+            }
         }
     }
 
-    
 }
